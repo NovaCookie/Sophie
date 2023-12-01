0cd2422532b4d31ddfebb41c7784f5fa2666e262
NovaCookie <95030874+NovaCookie@users.noreply.github.com>
2023-12-01 19:20:05 +0100

    feat : Affichage index en tant que administrateur

---
+++ FrontEnd/js/editionIndex.js
@@ -0,0 +1,11 @@
+var editBandeau = document.getElementById("editBnd");
+var editBtn = document.getElementById("openModal");
+
+const jeton_edition = window.sessionStorage.getItem('token');
+if (jeton_edition) {
+  editBtn.style.display = "flex";
+  editBandeau.style.display = "flex";
+} else {
+  editBandeau.style.display = "none";
+  editBtn.style.display = "none";
+}
