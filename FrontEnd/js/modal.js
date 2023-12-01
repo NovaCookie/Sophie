a96f6a657f66827c00934b6fcee60eec66fedb69
NovaCookie <95030874+NovaCookie@users.noreply.github.com>
2023-12-01 19:35:32 +0100

    feat : Prévisualisation nouvelle image

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -5,7 +5,11 @@ const btnAdd = document.getElementById("btnAdd");
 const arrow = document.getElementById("arrow_left")
 const fermer1 = document.getElementById("closeOne");
 const fermer2 = document.getElementById("closeTwo");
+const modal_gallery = document.getElementById("modal-gallery");
 const divAff = document.getElementById("div-img-aff")
+const img = document.getElementById("img-aff");
+const titre = document.getElementById("Titre")
+const categ = document.getElementById("Catégorie")
 
 openModal.onclick = function () {
     modal.style.display = "block";
@@ -38,4 +42,19 @@ window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+//Affiche l'image sélectionner dans les fichiers
+function changeImg() {
+    const imgSelected = document.getElementById("FileTransfert").files[0];
+    img.classList.add("obj");
+    img.file = imgSelected;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        img.src = e.target.result;
+    };
+    reader.readAsDataURL(imgSelected);
+    var divAff = document.getElementById("div-img-aff")
+    divAff.style.opacity = "1";
+    divAff.style.zIndex = "1";
 }
