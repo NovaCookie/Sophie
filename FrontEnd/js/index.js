8882df93c7c8d867ea976c2352976d87edeeeb22
NovaCookie <95030874+NovaCookie@users.noreply.github.com>
2024-03-26 12:11:44 +0100

    oublie fichier index .js

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,16 +1,25 @@
-const jeton_login = window.sessionStorage.getItem('token');
-if (jeton_login) {
+const tk = window.sessionStorage.getItem('token');
+let editBandeau = document.getElementById("editBnd");
+let editBtn = document.getElementById("openModal");
+
+if (tk) {
   if (loginId.innerHTML === "login") {
     loginId.innerHTML = "logout";
   } else {
     loginId.innerHTML = "login";
   }
-
+  editBtn.style.display = "flex";
+  editBandeau.style.display = "flex";
+} else {
+  editBandeau.style.display = "none";
+  editBtn.style.display = "none";
 }
-document.getElementById("loginId").addEventListener("click", loginout)
-function loginout() {
+
+document.getElementById("loginId").addEventListener("click", function () {
   if (loginId.innerHTML === "logout") {
     sessionStorage.clear();
     loginId.innerHTML = "login";
   }
 }
+)
+
