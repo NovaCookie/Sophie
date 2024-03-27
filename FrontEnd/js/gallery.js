d76d35a2a7a0e2ab73e60f3df21640f549408220
NovaCookie <95030874+NovaCookie@users.noreply.github.com>
2024-03-27 18:17:59 +0100

    reste

--- FrontEnd/js/gallery.js
+++ FrontEnd/js/gallery.js
@@ -1,6 +1,7 @@
-import { getImg, getCateg} from "./impoFetchApi.js";
-const parent = document.getElementById("filtre_id");
+import { getImg, getCateg } from "./impoFetchApi.js";
+const filtre_id = document.getElementById("filtre_id");
 const gallery_id = document.getElementById("gallery_id");
+const tk = window.sessionStorage.getItem('token');
 filtres();
 async function filtres() {
   createBtnFiltre("Tous", 0);
@@ -10,20 +11,23 @@ async function filtres() {
     });
 }
 function createBtnFiltre(name, id) {
-  const btn = document.createElement("button");
-  parent.insertAdjacentElement("beforeend", btn);
-  btn.insertAdjacentText("afterbegin", name);
-  if (id == 0) {
-    selectBtnFiltre(btn);
+  if (!tk) {
+    const btn = document.createElement("button");
+    filtre_id.insertAdjacentElement("beforeend", btn);
+    btn.insertAdjacentText("afterbegin", name);
+    if (id == 0) {
+      selectBtnFiltre(btn);
+    }
+    btn.addEventListener("click", () => {
+      selectBtnFiltre(btn);
+      lesFiltres(id);
+    })
+  } else {
+    filtre_id.style.margin = "2%";
   }
-  btn.addEventListener("click", () => {
-    selectBtnFiltre(btn);
-    lesFiltres(id);
-  })
 }
-
 function selectBtnFiltre(btn) {
-  const tableauChild = parent.childNodes;
+  const tableauChild = filtre_id.childNodes;
   tableauChild.forEach(btn => {
     if (btn.classList == "isSelected") {
       btn.classList.remove('isSelected');
@@ -60,7 +64,7 @@ async function lesFiltres(id) {
 //Création dynamique de balise et insère les images dedans
 export function addElement(url, name, id) {
 
-  
+
   const figure = document.createElement("figure");
   figure.id = `gallery_${id}`;
   let img = document.createElement("img");
@@ -77,9 +81,8 @@ export function addElement(url, name, id) {
 
 //Suppression de toutes les enfants de la balise contenant toutes les images
 function delElement() {
-  let parent = document.getElementById('gallery_id');
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+  while (gallery_id.firstChild) {
+    gallery_id.removeChild(gallery_id.firstChild);
   }
 }
 
